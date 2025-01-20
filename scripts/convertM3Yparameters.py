32ea053c321cf85446afb2a19605755c8957f3be
migueldelafuente1 <migueldelafuente4@gmail.com>
2025-01-20 12:06:16 +0100

    fixing M3Y parameters

--- scripts/convertM3Yparameters.py
+++ scripts/convertM3Yparameters.py
@@ -5,23 +5,25 @@ Created on 30 dic 2024
 '''
 import numpy as np
 
-## Matrix to pass from Bartlett, Majorana ... to P[TS]
+## Matrix to pass from P[TS] to Bartlett, Majorana ... (order of the outcomming operator)
 CONV_MATRIX = [
-    [1,  1, -1, -1],  # 1
-    [1, -1, -1,  1],  # -Heisenberg
-    [1,  1,  1,  1],  # Bartlett
-    [1, -1,  1, -1],  # -Majorana
+    #W, -H,  B, -M 
+    [1,  1, -1, -1],  # P 00 = SO
+    [1, -1, -1,  1],  # P 10 = SE 
+    [1,  1,  1,  1],  # P 01 = TE
+    [1, -1,  1, -1],  # P 11 = TO
 ]
-CONV_MATRIX = np.array(CONV_MATRIX) * 0.25
+CONV_MATRIX = np.array(CONV_MATRIX) 
 
 ## Matrix to pass from P[TS] (article ) to Bartlett, Majorana ... (inverse)
 CONV_INV_MATRIX = [
-    [ 1,  1,  1,  1],  # P 00 = SO
-    [ 1, -1,  1, -1],  # P 10 = SE
-    [-1, -1,  1,  1],  # P 01 = TE
-    [-1,  1,  1, -1],  # P 11 = TO
+    #SO  SE  TE  TO
+    [ 1,  1,  1,  1],  # 1
+    [ 1, -1,  1, -1],  # -Heisenberg
+    [-1, -1,  1,  1],  # Bartlett
+    [-1,  1,  1, -1],  # -Majorana
 ]
-CONV_INV_MATRIX = np.array(CONV_INV_MATRIX) 
+CONV_INV_MATRIX = np.array(CONV_INV_MATRIX) * 0.25
 
 ## TODO: Test if it is inverse
 print(np.dot(CONV_INV_MATRIX, CONV_MATRIX), "== unitary(4)\n")
@@ -106,6 +108,68 @@ def print_xml_elements(params_c, params_ls, params_t):
         
 if __name__ == '__main__':
     
+    # M3Y - P2
+    #===========================================================================
+    par_p2 = [
+        [-11900,  2730, 31.389,], # SO
+        [  8027, -2880,-10.463,], # SE
+        [  6080, -4266,-10.463,], # TE
+        [  3800,  -780,  3.488,], # TO
+        [  0.25,   0.4,  1.414,], # 1/mu
+    ]
+    par_p2_LS = [
+        [  0.0,  0.0], # SO
+        [  0.0,  0.0], # SE
+        [-9181.8,  -606.6], # TE
+        [-3414.6, -1137.6], # TO
+        [ 0.25, 0.40] # 1/mu
+    ]
+    par_p2_T = [
+        [    0.0, 0.0], # SO
+        [    0.0, 0.0], # SE
+        [-131.52,-3.708], # TE
+        [  29.28, 1.872], # TO
+        [   0.40, 0.70] # 1/mu 
+    ]
+    print("P2 - M3Y parametrization")
+    print_xml_elements(par_p2, par_p2_LS, par_p2_T)
+    print()
+    
+    # M3Y - P6
+    #===========================================================================
+    par_p2 = [
+        [  -728,  1386, 31.389,], # SO
+        [ 10766, -3520,-10.463,], # SE
+        [  8474, -4594,-10.463,], # TE
+        [ 12453, -1588,  3.488,], # TO
+        [  0.25,   0.4,  1.414,], # 1/mu
+    ]
+    par_p2_LS = [
+        [  0.0,  0.0], # SO
+        [  0.0,  0.0], # SE
+        [ -11222.2, -741.4], # TE
+        [  -4173.4,-1390.4], # TO
+        [ 0.25, 0.40] # 1/mu
+    ]
+    par_p2_T = [
+        [  0.0,  0.0], # SO
+        [  0.0,  0.0], # SE
+        [-1096, -30.9], # TE
+        [  244,  15.6], # TO
+        [   0.40, 0.70] # 1/mu 
+    ]
+    
+    par_p6_dens = [
+        [  0.0], # SO
+        [  384], # SE
+        [ 1930], # TE
+        [  0.0], # TO
+        ]
+    
+    print("P6 - M3Y parametrization")
+    print_xml_elements(par_p2, par_p2_LS, par_p2_T)
+    print()
+    
     # M3Y - P0
     #===========================================================================
     par_p0 = [
@@ -118,44 +182,18 @@ if __name__ == '__main__':
     par_p0_LS = [
         [  0.0,  0.0], # SO
         [  0.0,  0.0], # SE
-        [-1897, -632], # TO
         [-5101, -337], # TE
+        [-1897, -632], # TO
         [ 0.25, 0.40] # 1/mu
     ]
     par_p0_T = [
         [  0.0,  0.0], # SO
         [  0.0,  0.0], # SE
-        [  244, 15.6], # TO
         [-1096,-30.9], # TE
+        [  244, 15.6], # TO
         [ 0.40,  0.70] # 1/mu 
     ]
     print("P0 - M3Y parametrization")
     print_xml_elements(par_p0, par_p0_LS, par_p0_T)
     print()
     
-    # M3Y - P2
-    #===========================================================================
-    par_p2 = [
-        [-11900,  2730, 31.389,], # SO
-        [  8027, -2880,-10.463,], # SE
-        [  6080, -4266,-10.463,], # TE
-        [  3800,  -780,  3.488,], # TO
-        [  0.25,   0.4,  1.414,], # 1/mu
-    ]
-    par_p2_LS = [
-        [-3414.6, -1137.6], # SO
-        [-9181.8,  -606.6], # SE
-        [  0.0,  0.0], # TO
-        [  0.0,  0.0], # TE
-        [ 0.25, 0.40] # 1/mu
-    ]
-    par_p2_T = [
-        [    0.0, 0.0], # SO
-        [    0.0, 0.0], # SE
-        [  29.28, 1.872], # TO
-        [-131.52,-3.708], # TE
-        [   0.40, 0.70] # 1/mu 
-    ]
-    print("P2 - M3Y parametrization")
-    print_xml_elements(par_p2, par_p2_LS, par_p2_T)
-    print()
