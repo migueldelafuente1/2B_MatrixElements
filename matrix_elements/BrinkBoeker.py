5026cba5af5c3ade7655cb4009c69cd282802a31
migueldelafuente1 <migueldelafuente4@gmail.com>
2026-03-03 20:38:50 +0100

    adding multipolar test interaction for Brink Boeker

--- matrix_elements/BrinkBoeker.py
+++ matrix_elements/BrinkBoeker.py
@@ -5,7 +5,8 @@ Created on Feb 23, 2021
 '''
 import numpy as np
 
-from matrix_elements.MatrixElement import _TwoBodyMatrixElement_JTCoupled
+from matrix_elements.MatrixElement import _TwoBodyMatrixElement_JTCoupled,\
+    _TwoBodyMatrixElement_Antisym_JTCoupled
 from matrix_elements.transformations import TalmiTransformation
 
 from helpers.Enums import BrinkBoekerParameters as bb_p, CentralMEParameters,\
@@ -15,10 +16,10 @@ from helpers.Enums import SHO_Parameters
 from helpers.integrals import talmiIntegral
 
 from helpers.Log import XLog
-from helpers.Helpers import safe_wigner_6j, safe_clebsch_gordan
-from sympy.physics.wigner import clebsch_gordan
+from helpers.Helpers import safe_wigner_6j, safe_clebsch_gordan, safe_wigner_9j,\
+    safe_3j_symbols, fact, double_factorial, gamma_half_int
 
-class BrinkBoeker(_TwoBodyMatrixElement_JTCoupled, TalmiTransformation):
+class BrinkBoeker_BrodyMoshinsky(_TwoBodyMatrixElement_JTCoupled, TalmiTransformation):
     
     """
     Implementation of the central force for two gaussians_, overriding of the
@@ -170,7 +171,207 @@ class BrinkBoeker(_TwoBodyMatrixElement_JTCoupled, TalmiTransformation):
                            exch_sum=sum(exchange_energy), val=prod_part)
         
         return aux_sum 
+
+
+class BrinkBoeker_Multipolar(_TwoBodyMatrixElement_Antisym_JTCoupled, 
+                             BrinkBoeker_BrodyMoshinsky):
+    
+    
+    K_max = 10
+    
+    def _LScoupled_MatrixElement(self):
+        """ 
+        <(n1,l1)(n2,l2) (LS)| V |(n1,l1)'(n2,l2)'(L'S') (T)>
+        """
+        if (self.bra.shellStatesNotation == '0s1/2 0s1/2' and 
+            self.ket.shellStatesNotation == '0p1/2 0p1/2'):
+            _ = 0
+        
+        aux_sum = 0.0
+        # Sum of gaussians and projection operators
+        for i in range(self.numberGaussians):
+            self._part = i
             
+            # Radial Part for Gaussian Integral
+            radial_energy = self.multipolarEvaluation()
+            
+            # Exchange Part
+            # W + P(S)* B - P(T)* H - P(T)*P(S)* M
+            _S_aux = (-1)**(self.S_bra + 1)
+            _T_aux = (-1)**(self.T)
+            _L_aux = (-1)**(self.T + self.S_bra + 1)
+            
+            exchange_energy = (
+                self.PARAMS_FORCE[i].get(bb_p.Wigner),
+                self.PARAMS_FORCE[i].get(bb_p.Bartlett)   * _S_aux,
+                self.PARAMS_FORCE[i].get(bb_p.Heisenberg) * _T_aux,
+                self.PARAMS_FORCE[i].get(bb_p.Majorana)   * _L_aux
+            )
+            
+            # Add up
+            prod_part = radial_energy * sum(exchange_energy)
+            aux_sum += prod_part
+            
+            if self.DEBUG_MODE:
+                XLog.write('BB', radial=radial_energy, exch=exchange_energy, 
+                           exch_sum=sum(exchange_energy), val=prod_part)
+        
+        return aux_sum
+    
+    def _validKetTotalAngularMomentums(self):
+        return (self.L_bra, )
+    def _validKetTotalSpins(self):
+        return (self.S_bra, )
+    
+    @classmethod
+    def _setBiggerKMAX(cls, k_new):
+        cls.K_max = max(k_new, cls.K_max)
+    
+    def multipolarEvaluation(self):
+        """ 
+        Evaluates  W_0(r1,r2) Y0*Y0 + W_1(r1,r2) Y1*Y1 + ...  matrix element
+        """ 
+        V_gauss = [0, ]*(self.K_max + 1)
+        k_max = min(self.bra.l1 + self.ket.l1, self.bra.l2 + self.ket.l2)
+        if k_max > self.K_max: self._setBiggerKMAX(k_max) 
+        
+        for k in range(self.K_max +1):            
+            if ((self.bra.l1 + self.ket.l1 + k)%2 or (self.bra.l2 + self.ket.l2 + k)%2): 
+                continue
+            
+            const_quad = 1 #0.6 if k == 2 else 1.0
+                
+            angular = self._angularCoefficient(k)
+            if self.isNullValue(angular): continue
+            
+            radial  = self._radialIntegrals(k) #* self._radialIntegrals(k, 2) 
+            
+            V_gauss[k] = angular * radial * const_quad
+        
+        return sum(V_gauss)
+    
+    _angular_coeffs = {}
+    @classmethod
+    def _get_keyAngular(cls,  l1, l2, l1q, l2q, L, k):
+        return "{}_{}_{}_{}_{}_{}".format(l1, l2, l1q, l2q, L, k)
+    
+    @classmethod
+    def _save_angularCoeff(cls, l1, l2, l1q, l2q, L, k, value):
+        key_ = cls._get_keyAngular(l1, l2, l1q, l2q, L, k)
+        cls._angular_coeffs[key_] = value
+    
+    @classmethod
+    def _get_angularCoeff(cls,  l1, l2, l1q, l2q, L, k):
+        key_ = cls._get_keyAngular(l1, l2, l1q, l2q, L, k)
+        if key_ in cls._angular_coeffs:
+            return cls._angular_coeffs[key_]
+        return None
+    
+    
+    def _angularCoefficient(self, k):
+        val_prev = self._get_angularCoeff(self.bra.l1, self.bra.l2, 
+                                          self.ket.l1, self.ket.l2, self.L_bra, k)
+        if val_prev: return val_prev
+        
+        factor = safe_wigner_6j(self.bra.l1, self.bra.l2, self.L_bra,
+                                self.ket.l2, self.ket.l1, k)
+        factor *= (-1)**(self.bra.l2 + self.ket.l1 + self.L_bra) #* (np.sqrt(2*self.L_bra + 1))
+        
+        aux = np.sqrt( (2*self.ket.l1 + 1)*(2*self.ket.l2 + 1)*
+                      ((2*self.bra.l1 + 1)*(2*self.bra.l2 + 1))) * (2*k + 1) / (4*np.pi)
+        aux *= safe_3j_symbols(self.bra.l1, k, self.ket.l1, 0, 0, 0)
+        aux *= safe_3j_symbols(self.bra.l2, k, self.ket.l2, 0, 0, 0)
+        aux *= (-1)**(self.bra.l1 + self.bra.l2)
+        
+        value = factor * aux 
+        
+        self._save_angularCoeff(self.bra.l1, self.bra.l2, 
+                                self.ket.l1, self.ket.l2, self.L_bra, k, value)
+        return value    
+    
+    
+    S_RANGE_MAX = 30
+    _radial_Coeffs = {}
+    @classmethod
+    def _radialAuxCoeff(cls, k, s, part):
+        if not (part, k, s) in cls._radial_Coeffs:
+            val = - gamma_half_int(2*(s + k) + 3) - fact(s)
+            cls._radial_Coeffs[(part, k, s)] = np.exp(val) * np.sqrt(np.pi) / 2
+        return cls._radial_Coeffs[(part, k, s)]
+    
+    def _radialIntegrals(self, k):
+        sum_ = 0
+        
+        for s in range(self.S_RANGE_MAX +1):
+            
+            sum_s = [0, 0]
+            for term in (1, 2):
+                n ,l  = getattr(self.bra, f'n{term}'), getattr(self.bra, f'l{term}')
+                nq,lq = getattr(self.ket, f'n{term}'), getattr(self.ket, f'l{term}')
+                
+                self._n   = n
+                self._n_q = nq
+                self._l   = l
+                self._l_q = lq
+                    
+                sum_s[term - 1] = self._integrals1B(k,  s)
+            
+            coeff_s = self._radialAuxCoeff(k, s, self._part)
+            
+            sum_ += coeff_s * sum_s[0] * sum_s[1]
+        
+        return sum_ * (4*np.pi)
+    
+    def _normConstantSHO(self):
+        factor = 2 / (self.PARAMS_SHO[SHO_Parameters.b_length]**3)
+        aux = 0.5 * (fact( self._n) + fact( self._n_q)
+                     - gamma_half_int(2*(self._n + self._l) + 3 )
+                     - gamma_half_int(2*(self._n_q + self._l_q) + 3 ) 
+        )
+        return factor * np.exp(aux)  
+    
+    def _LaguerreCoefficient(self, m, n, l):
+        
+        aux = (
+            gamma_half_int(2*(n + l) + 3 )
+            - (gamma_half_int(2*(l + m) + 3 ) + fact(n - m) + fact(m))
+        )
+        return (-1)**m * np.exp(aux)
+        
+        
+    def _integrals1B(self, k, s):
+        
+        b_len   = self.PARAMS_SHO.get(SHO_Parameters.b_length)
+        mu_len  = self.PARAMS_FORCE[self._part].get(CentralMEParameters.mu_length)
+        A       = (1/b_len**2) + (1/mu_len**2)
+        
+        KS = 2*s + k
+        
+        sum_ = 0
+        for m1 in range(self._n +1):
+            c1_ = self._LaguerreCoefficient(m1, self._n, self._l)
+            for m2 in range(self._n_q +1):
+                c2_ = self._LaguerreCoefficient(m2, self._n_q, self._l_q)
+                
+                K = 2*(m1 + m2) + (self._l + self._l_q) + KS + 2
+                
+                factor = (
+                    gamma_half_int(K + 1)
+                    + np.log(b_len / mu_len) * (KS)
+                    - np.log(b_len) * (K - 2)
+                    - np.log(A) * ((K + 1)/2)
+                )
+                
+                sum_ += c1_ * c2_ * 0.5 * np.exp(factor)
+        
+        return sum_ * self._normConstantSHO()
+    
+
+class BrinkBoeker(
+        BrinkBoeker_BrodyMoshinsky,
+        # BrinkBoeker_Multipolar,
+        ):
+    pass
 
 
 class PotentialSeries_JTScheme(BrinkBoeker):
